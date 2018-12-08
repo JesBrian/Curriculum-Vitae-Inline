96a4b27c9e101ac30b4fbd436d41faa73da245c2
JesBrian <JesBrian666@gmail.com>
2018-12-08 21:57:15 +0800

    server - [ try to build the frame ]

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,13 @@
-console.log(888);
+
+const Koa = require('koa');
+const app = new Koa();
+const registerRouter = require('./Router/index.js');
+
+app.use(async (ctx, next) => {
+  ctx.body = 'Hello World';
+  next();
+});
+
+app.use(registerRouter())
+
+app.listen(3000);
