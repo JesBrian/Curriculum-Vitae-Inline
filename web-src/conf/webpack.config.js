db2a2a3010e50ddcd267d0f34234cde44936bcbd
JesBrian <JesBrian666@gmail.com>
2018-12-17 00:10:55 +0800

    project - [ try to rebuild web ]

---
+++ web-src/conf/webpack.config.js
@@ -0,0 +1,24 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  entry: {
+    index: "./main.js"  //webpack的入口文件，指定将哪个文件进行打包
+  },
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      use: ['vue-loader']
+    }, {
+      test: /\.js$/,
+      use: ['babel-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loaders: ["style-loader", "css-loader"]
+    }]
+  },
+  plugins: [
+    // 添加VueLoaderPlugin，以响应vue-loader
+    new VueLoaderPlugin()
+  ]
+};
