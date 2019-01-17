4e6579c7599880644a17cee3d101adc92d518317
JesBrian <JesBrian666@gmail.com>
2019-01-18 00:01:36 +0800

    web - [ before merge commit ]

---
+++ web/src/components/SuperSearch/SuperSearch.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'SuperSearch'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- web/src/views/Home/SubPages/CellManagement/CellManagement.vue
+++ web/src/views/Home/SubPages/CellManagement/CellManagement.vue
@@ -10,12 +10,14 @@
       </div>
     </PathNavbar>
 
-    <div style="padding:0 18px 0 38px;">
+    <div style="padding:8px 18px 0 38px; position:relative;">
       <Tabs type="card">
-        <TabPane label="标签一">标签一的内容</TabPane>
-        <TabPane label="标签二">标签二的内容</TabPane>
-        <TabPane label="标签三">标签三的内容</TabPane>
+        <TabPane label="个人组件">标签一的内容</TabPane>
+        <TabPane label="收藏组件">标签二的内容</TabPane>
+        <TabPane label="网络组件">标签三的内容</TabPane>
       </Tabs>
+
+      <Input search placeholder="请输入关键字..." style="width:238px; top:0; right:18px; position:absolute;" />
     </div>
   </div>
 </template>
