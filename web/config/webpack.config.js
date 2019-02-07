028a6174d5693828f689cabc9d84976a206c2958
JesBrian <jesbrian666@gmail.com>
2019-02-08 00:48:54 +0800

    config - [ update iview-loader ]

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -10,7 +10,15 @@ module.exports = {
   module: {
     rules: [{
       test: /\.vue$/,
-      use: ['vue-loader']
+      use: [
+        'vue-loader',
+        {
+          loader: 'iview-loader',
+          options: {
+            prefix: false
+          }
+        }
+      ]
     }, {
       test: /\.js$/,
       use: ['babel-loader'],
