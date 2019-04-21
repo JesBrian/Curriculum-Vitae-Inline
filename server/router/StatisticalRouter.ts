8c2871f970436c2b06b0080e087f8f41d62b899c
JesBrian <jesbrian666@gmail.com>
2019-04-21 17:33:12 +0800

    server - [ init statistical module ]

---
+++ server/router/StatisticalRouter.ts
@@ -0,0 +1,10 @@
+const {
+} = require('../controller/StatisticalController');
+
+// @ts-ignore
+const Router = require('koa-router');
+
+// @ts-ignore
+const router = new Router();
+
+module.exports = router;
